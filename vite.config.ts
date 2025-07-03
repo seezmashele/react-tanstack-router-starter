b1801fd546ab43de71c0a1b1f0c33b5d3e8fc53f
seezmashele <seez.mashele@gmail.com>
2025-07-03 18:46:04 +0200

    config touchups

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,7 +20,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
